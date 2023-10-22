f99557b69c7a14fb213241058d6ebd6ca83c47fe
Kevin <Kevin@DESKTOP-27L22BB>
2023-10-22 13:38:04 +0200

    Add project files.

---
+++ uppgift4_9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift4_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange triangelns sidolängd");
+            int sido=int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sido; i++) 
+            {
+                for (int j = 1; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
